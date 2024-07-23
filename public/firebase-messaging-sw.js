abcceb56175038248eaf5dfabae644c22c3836c8
anupam-dixit <69384581+anupam-dixit@users.noreply.github.com>
2024-07-23 23:03:05 +0530

    h89viuguio

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,3 +1,5 @@
+import {initializeApp} from "@firebase/app";
+
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
